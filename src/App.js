394c651e32268c192b6aabea258d13caa8ce736d
Yuvraj Chhetri <92994932+i-am-yuvi@users.noreply.github.com>
2023-05-05 06:28:55 +0000

    added heading and textbox

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TextField from '@mui/material/TextField';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hi, Welcome to Synthetic.ai</h1>
+      
+      <TextField className="textbox-link" label="Enter link" variant="outlined" />
+    </div>
+  );
+}
+
+export default App;
