8c0697f5a697d718bb28207266b32468721b45a8
Yuvraj Chhetri <92994932+i-am-yuvi@users.noreply.github.com>
2023-05-15 14:31:04 +0000

    added dropdown function

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
 import TextField from '@mui/material/TextField';
 import "./Home.css"
 import logo from "./logo.jpg";
+import Dropdown from "./Dropdown";
 
 
 function Home() {
+
+    const options = [
+        {value:"tweet", label:"Tweet"},
+        {value:"twitter-thread", label:"Twitter Thread"},
+        {value:"instagram-caption", label:"Instagram Caption"},
+        {value:"linkedIn-post", label:"LinkedIn Post"},
+        {value:"blog-ideas", label:"Blog Ideas"},
+        {value:"blog-outline", label:"Blog Outline"},
+        {value:"summary", label:"Summary"},
+        {value:"rewrite-professional", label:"Rewrite in Professional Tone"},
+        {value:"rewrite-empathetic", label:"Rewrite in Empathetic Tone"},
+        {value:"rewrite-friendly", label:"Rewrite in Friendly Tone"},
+        {value:"rewrite-casual", label:"Rewrite in Casual Tone"},
+        {value:"expand-content", label:"Expand the Content"},
+    ]
     return(
         <div className="home">
             {/*logo & name on top left corner*/}
@@ -12,7 +28,15 @@ function Home() {
                 <span className="aiName">Synthetic.ai</span>
             </div>
 
+            <h2 className="titleName">Enter the link below </h2>
+
             <TextField className="textbox-link" label="Enter link" variant="outlined"/>
+
+            <h2 className="dropdownTitle">Now Select the Content from the Dropdown!</h2>
+
+            <div className="dropdown-items">
+                <Dropdown placeHolder="Select..." options={options}/>
+            </div>
 
             <div className="btn">
                 <button className="submitBtn">Submit</button>
